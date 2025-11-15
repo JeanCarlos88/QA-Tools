8a2052a24ee1cc50250902ce9b46b54ce82851fc
Felipe Freires <felipeplayf13@gmail.com>
2025-11-15 17:13:13 -0300

    Fix: correção de tags no Header.jsx

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -8,35 +8,40 @@ const Header = ({ theme, toggleTheme }) => {
     dark:bg-dark-900 dark:border-dark-800 dark:text-gray-100 ">
       <div className="container mx-auto px-4 py-4">
         <div className="flex items-center justify-between">
-        <div className="flex items-center justify-between space-x-4">
-          <Link to="/" className="flex items-center  space-x-2 hover:opacity-80 transition-opacity">
-            <span className="text-2xl">🛠️</span>
-            <h1 className="text-2xl font-bold bg-gradient-to-r from-primary-400 to-primary-600 bg-clip-text text-transparent">{APP_NAME}</h1>
-          </Link>
-        </div>
-        <div className="flex items-center space-x-4">
 
+          <div className="flex items-center justify-between space-x-4">
+            <Link to="/" className="flex items-center space-x-2 hover:opacity-80 transition-opacity">
+              <span className="text-2xl">🛠️</span>
+              <h1 className="text-2xl font-bold bg-gradient-to-r from-primary-400 to-primary-600 bg-clip-text text-transparent">
+                {APP_NAME}
+              </h1>
+            </Link>
+          </div>
+
+          <div className="flex items-center space-x-4">
             <ThemeToggle theme={theme} toggleTheme={toggleTheme} />
 
-          <nav className="hidden md:flex space-x-6">
-            <a 
-              href="https://github.com/JeanCarlos88/QA-Tools" 
-              target="_blank" 
-              rel="noopener noreferrer"
-              className="hover:text-primary-400 transition-colors"
-            >
-              GitHub
-            </a>
-            <a 
-              href="https://github.com/JeanCarlos88/QA-Tools/issues" 
-              target="_blank" 
-              rel="noopener noreferrer"
-              className="hover:text-primary-400 transition-colors"
-            >
-              Reportar Bug
-            </a>
-          </nav>
-        </div>
+            <nav className="hidden md:flex space-x-6">
+              <a
+                href="https://github.com/JeanCarlos88/QA-Tools"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:text-primary-400 transition-colors"
+              >
+                GitHub
+              </a>
+
+              <a
+                href="https://github.com/JeanCarlos88/QA-Tools/issues"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:text-primary-400 transition-colors"
+              >
+                Reportar Bug
+              </a>
+            </nav>
+
+          </div>
         </div>
       </div>
     </header>
